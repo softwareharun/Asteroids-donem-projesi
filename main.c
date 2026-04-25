fe37206ad1dd8e0af07ccd906d8ab9cae0390b65
Harun Bozkurt <harunbzkrt78@gmail.com>
2026-04-25 20:26:47 +0300

    pencere acmayi ve kapatmayi fonksiyonlara yazdim bu sayede mainin icindeki kodlar azaldi goruntu guzellesti

--- main.c
+++ main.c
@@ -8,15 +8,13 @@ const int pencereGenisligi = 700;
 SDL_Window* pencere = NULL; //penceremizi ve ekranyüzeyimizi tanýmlýyoruz bunlarý pointer ile tanýmlama sebebimiz bunlarýn aslýnda devasa bir struck yapýsý olmasýdýr main fonksiyonumuzda her çaðýrdýgýmýzda hepsinin çaðrýlmasý degil sadece o konumun gönderilmesidir. null atama sebebimiz ise pointer tanýmladýgmýz icin bize boþ bir adres tutmasýný saglamak.
 SDL_Surface* ekranYuzeyi = NULL;
 
-	int main(int argc, char* args[]) //mainimizi açýyoruz fakat parantez içlerine dýþardan uygulamayý açarken gelicek olan komutlarýn sayýsýný tutmak icin int argc, dýþardan gelen komutlarýn ne oldugunu tutabilcegimiz bir char pointer dizisi olusturuyoruz.  
-//pointer þeklinde olusturmamýzýn sebebi ise birden fazla string yapýsýný tutabilmek
+bool pencereyiAC()//pencereyi açmayý ve sdl yi baþlatmayi bir fonksiyonla yapýyoruz mainin icindeki karmasa azalýyor
 {
-
-	SDL_Init(SDL_INIT_VIDEO); //burada SDL mizi görüntü ve penceremizi baþlatýyoruz.
-
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)//burada SDL mizi görüntü ve penceremizi baþlatýyoruz.
 	{
 		printf("Dosya aciliamadi.. SDL hatasi :%s\n", SDL_GetError()); //eger bir hata ile karsilasirsak geterror fonksiyonu ile bize hatanýn sebebini alýyoruz
+		return false; //dosya açýlamazsa false döndür
 	}
 
 	pencere = SDL_CreateWindow("AsteroidsGame", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, pencereGenisligi, pencereUzunlugu, SDL_WINDOW_SHOWN); //penceremizi olusturuyoruz ilk parametremiz pencerenin üst cugubunda yazýcak olan isim, ikincisi pencerenin yatay konumda nerden baþlayacagý, üçüncüsü dikey konumda, dört ve besinciler ise pencremizin uzunlugu ve genisligi, sdl window shown yazarak da olusturur olusturmaz pencerenin açýlmasýný saglamak.
@@ -24,9 +22,26 @@ SDL_Surface* ekranYuzeyi = NULL;
 	if (pencere == NULL)
 	{
 		printf("Pencere olusturualamdi... Hata :%s\n", SDL_GetError()); //ayný sekilde pencere acilamama durumunda hatanin sebebini almak icin 
+		return false; //pencere olusmazsa false döndür
 	}
 
 	ekranYuzeyi = SDL_GetWindowSurface(pencere); // penceremizi olusturuyoruz
+	return true;
+}
+void pencereyiKapat()//pencereyi kapatmayi da bir fonksiyona atiyoruz mainde bunu çaðýrmamýz yeticek
+{
+	SDL_DestroyWindow(pencere); //olusturdugumuz pencereyi kapatayiyoruz
+	SDL_Quit(); //baþlattýgýmýz sdl lerin hepsini kapatiyoruz
+}
+	int main(int argc, char* args[]) //mainimizi açýyoruz fakat parantez içlerine dýþardan uygulamayý açarken gelicek olan komutlarýn sayýsýný tutmak icin int argc, dýþardan gelen komutlarýn ne oldugunu tutabilcegimiz bir char pointer dizisi olusturuyoruz.  
+//pointer þeklinde olusturmamýzýn sebebi ise birden fazla string yapýsýný tutabilmek
+{
+		if (!pencereyiAC())
+		{
+			printf("Pencere veya dosya acilamadi..");
+			
+		}
+
 	SDL_FillRect(ekranYuzeyi, NULL, SDL_MapRGB(ekranYuzeyi->format, 0xFF, 0xFF, 0xFF)); // ekranyüzeyin yüzeyinin boyamak istegimiz alanýný boyuyoruz null yazarak hepsini boyuyoruz .format yaparak ise rgb formatýna sabitliyoruz.
 
 
@@ -45,9 +60,8 @@ SDL_Surface* ekranYuzeyi = NULL;
 	
 		SDL_UpdateWindowSurface(pencere); //her tusa basýldýktan sonra oyun penceresini guncelliyoruz
 	}
-
-	SDL_DestroyWindow(pencere); //olusturdugumuz pencereyi kapatayiyoruz
-	SDL_Quit; //baþlattýgýmýz sdl lerin hepsini kapatiyoruz
+	
+	pencereyiKapat();
 
 	return 0;
 }
